ad1ab382af9e3250115d856adb1bef80c00f241f
hlin863 <zcabhli@ucl.ac.uk>
2023-09-21 21:57:24 +0100

    Drew US states maps
    Each neighboring states have differing colors.

--- draw_dynamic.py
+++ draw_dynamic.py
@@ -1,182 +1,242 @@
-# use dynamic programming to color each US state such that it has different colors to its neighbours.
+import networkx as nx
 import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap
-from matplotlib.patches import Polygon
 
 
-def is_valid_color(v, color, graph, colors_assigned):
-    for neighbour in graph[v]:
-        if colors_assigned[neighbour] == color:
-            return False
-    return True
+def draw_colored_us_map():
+    G = nx.Graph()
+
+    # create a dictionary mapping between states and state codes
+    state_dictionary = {
+        "Alabama": "AL",
+        "Alaska": "AK",
+        "Arizona": "AZ",
+        "Arkansas": "AR",
+        "California": "CA",
+        "Colorado": "CO",
+        "Connecticut": "CT",
+        "Delaware": "DE",
+        "Florida": "FL",
+        "Georgia": "GA",
+        "Hawaii": "HI",
+        "Idaho": "ID",
+        "Illinois": "IL",
+        "Indiana": "IN",
+        "Iowa": "IA",
+        "Kansas": "KS",
+        "Kentucky": "KY",
+        "Louisiana": "LA",
+        "Maine": "ME",
+        "Maryland": "MD",
+        "Massachusetts": "MA",
+        "Michigan": "MI",
+        "Minnesota": "MN",
+        "Mississippi": "MS",
+        "Missouri": "MO",
+        "Montana": "MT",
+        "Nebraska": "NE",
+        "Nevada": "NV",
+        "New Hampshire": "NH",
+        "New Jersey": "NJ",
+        "New Mexico": "NM",
+        "New York": "NY",
+        "North Carolina": "NC",
+        "North Dakota": "ND",
+        "Ohio": "OH",
+        "Oklahoma": "OK",
+        "Oregon": "OR",
+        "Pennsylvania": "PA",
+        "Rhode Island": "RI",
+        "South Carolina": "SC",
+        "South Dakota": "SD",
+        "Tennessee": "TN",
+        "Texas": "TX",
+        "Utah": "UT",
+        "Vermont": "VT",
+        "Virginia": "VA",
+        "Washington": "WA",
+        "West Virginia": "WV",
+        "Wisconsin": "WI",
+        "Wyoming": "WY"
+    }
+
+    # List of US states
+    states = [
+        "WA", "OR", "CA", "ID", "NV", "AZ", "UT", "MT", "WY", "CO", "NM", "ND", "SD", "NE", "KS",
+        "OK", "TX", "MN", "IA", "MO", "AR", "LA", "WI", "IL", "MS", "IN", "KY", "TN", "AL", "FL",
+        "GA", "SC", "NC", "VA", "WV", "OH", "MI", "ME", "NH", "VT", "NY", "MA", "CT", "RI", "PA",
+        "DE", "MD", "NJ", "AK", "HI"
+    ]
+
+    for state in states:
+        G.add_node(state)
+
+    # Neighboring states
+    borders = [
+        ("WA", "OR"), ("WA", "ID"), ("OR", "CA"), ("OR",
+                                                   "ID"), ("OR", "NV"), ("CA", "NV"),
+        ("CA", "AZ"), ("ID", "MT"), ("ID", "WY"), ("ID",
+                                                   "UT"), ("ID", "NV"), ("NV", "UT"),
+        ("NV", "AZ"), ("AZ", "UT"), ("AZ", "CO"), ("AZ",
+                                                   "NM"), ("UT", "WY"), ("UT", "CO"),
+        ("MT", "WY"), ("MT", "ND"), ("MT", "SD"), ("WY",
+                                                   "SD"), ("WY", "NE"), ("WY", "CO"),
+        ("CO", "NE"), ("CO", "KS"), ("CO", "OK"), ("CO",
+                                                   "NM"), ("NM", "TX"), ("NM", "OK"),
+        ("ND", "SD"), ("SD", "NE"), ("SD", "IA"), ("SD",
+                                                   "MN"), ("NE", "IA"), ("NE", "MO"),
+        ("NE", "KS"), ("KS", "MO"), ("KS", "OK"), ("OK",
+                                                   "MO"), ("OK", "AR"), ("OK", "TX"),
+        ("TX", "AR"), ("TX", "LA"), ("MN", "IA"), ("MN",
+                                                   "WI"), ("IA", "WI"), ("IA", "IL"),
+        ("IA", "MO"), ("MO", "IL"), ("MO", "KY"), ("MO",
+                                                   "TN"), ("MO", "AR"), ("AR", "LA"),
+        ("AR", "MS"), ("AR", "TN"), ("LA", "MS"), ("WI",
+                                                   "IL"), ("WI", "MI"), ("IL", "IN"),
+        ("IL", "KY"), ("MS", "TN"), ("MS", "AL"), ("IN",
+                                                   "MI"), ("IN", "OH"), ("IN", "KY"),
+        ("KY", "OH"), ("KY", "WV"), ("KY", "VA"), ("KY",
+                                                   "TN"), ("TN", "VA"), ("TN", "NC"),
+        ("TN", "GA"), ("TN", "AL"), ("AL", "GA"), ("AL",
+                                                   "FL"), ("GA", "FL"), ("GA", "SC"),
+        ("GA", "NC"), ("SC", "NC"), ("NC", "VA"), ("VA",
+                                                   "WV"), ("VA", "MD"), ("VA", "DE"),
+        ("WV", "OH"), ("WV", "PA"), ("WV", "MD"), ("OH",
+                                                   "PA"), ("MI", "OH"), ("ME", "NH"),
+        ("NH", "VT"), ("NH", "MA"), ("VT", "NY"), ("VT",
+                                                   "MA"), ("NY", "MA"), ("NY", "CT"),
+        ("NY", "RI"), ("NY", "PA"), ("NY", "NJ"), ("MA",
+                                                   "CT"), ("MA", "RI"), ("CT", "RI"),
+        ("CT", "NY"), ("RI", "NY"), ("PA", "NJ"), ("DE", "NJ"), ("DE", "MD")
+    ]
+
+    for state in states:
+        G.add_node(state)
+    for border in borders:
+        G.add_edge(*border)
+
+    # Color the graph
+    color_map = nx.coloring.greedy_color(G, strategy="largest_first")
+
+    # Create the Basemap instance for the continental US
+    m = Basemap(llcrnrlon=-119, llcrnrlat=20, urcrnrlon=-64, urcrnrlat=49,
+                projection='lcc', lat_1=33, lat_2=45, lon_0=-95)
+
+    fig, ax = plt.subplots(figsize=(12, 8))
+    m.drawcoastlines()
+    m.drawcountries()
+    m.drawmapboundary(fill_color='aqua')
+    m.fillcontinents(color='white', lake_color='aqua')
+
+    m.drawparallels(range(25, 65, 20), labels=[1, 0, 0, 0])
+    m.drawmeridians(range(-120, -40, 20), labels=[0, 0, 0, 1])
+
+    # Read the US states shape files and draw them on the map
+    m.readshapefile('st99_d00', name='states', drawbounds=True)
+
+    # Iterate over the states and color them
+    for info, shape in zip(m.states_info, m.states):
+        # print(info)
+        state_name = info['NAME']
+
+        # check if state name is in state dictionary
+        if state_name not in state_dictionary:
+            continue
+        # get state code from state dictionary using state name
+        state_code = state_dictionary[state_name]
+        print(state_code)
+        print(color_map)
+        if state_code in color_map:
+            # Assuming maximum 4 colors, you can adjust accordingly
+            color = plt.cm.rainbow(color_map[state_code] / 4.0)
+            poly = plt.Polygon(shape, facecolor=color, edgecolor=color)
+            ax.add_patch(poly)
+
+    plt.show()
 
 
-def graph_coloring_util(graph, m, colors_assigned, v):
-    if v == len(graph):
-        return True
+draw_colored_us_map()
 
-    for color in range(1, m + 1):
-        if is_valid_color(v, color, graph, colors_assigned):
-            colors_assigned[v] = color
-            if graph_coloring_util(graph, m, colors_assigned, v + 1):
-                return True
-            colors_assigned[v] = 0
+###
+# def draw_colored_us_map():
+#     G = nx.Graph()
 
-    return False
+#     # List of US states
+#     states = [
+#         "WA", "OR", "CA", "ID", "NV", "AZ", "UT", "MT", "WY", "CO", "NM", "ND", "SD", "NE", "KS",
+#         "OK", "TX", "MN", "IA", "MO", "AR", "LA", "WI", "IL", "MS", "IN", "KY", "TN", "AL", "FL",
+#         "GA", "SC", "NC", "VA", "WV", "OH", "MI", "ME", "NH", "VT", "NY", "MA", "CT", "RI", "PA",
+#         "DE", "MD", "NJ", "AK", "HI"
+#     ]
 
+#     for state in states:
+#         G.add_node(state)
 
-# def graph_coloring(graph, m):
-#     colors_assigned = [-1] * len(graph)
-#     if not graph_coloring_util(graph, m, colors_assigned, 0):
-#         return False
-#     return colors_assigned
+#     # Neighboring states
+#     borders = [
+#         ("WA", "OR"), ("WA", "ID"), ("OR", "CA"), ("OR",
+#                                                    "ID"), ("OR", "NV"), ("CA", "NV"),
+#         ("CA", "AZ"), ("ID", "MT"), ("ID", "WY"), ("ID",
+#                                                    "UT"), ("ID", "NV"), ("NV", "UT"),
+#         ("NV", "AZ"), ("AZ", "UT"), ("AZ", "CO"), ("AZ",
+#                                                    "NM"), ("UT", "WY"), ("UT", "CO"),
+#         ("MT", "WY"), ("MT", "ND"), ("MT", "SD"), ("WY",
+#                                                    "SD"), ("WY", "NE"), ("WY", "CO"),
+#         ("CO", "NE"), ("CO", "KS"), ("CO", "OK"), ("CO",
+#                                                    "NM"), ("NM", "TX"), ("NM", "OK"),
+#         ("ND", "SD"), ("SD", "NE"), ("SD", "IA"), ("SD",
+#                                                    "MN"), ("NE", "IA"), ("NE", "MO"),
+#         ("NE", "KS"), ("KS", "MO"), ("KS", "OK"), ("OK",
+#                                                    "MO"), ("OK", "AR"), ("OK", "TX"),
+#         ("TX", "AR"), ("TX", "LA"), ("MN", "IA"), ("MN",
+#                                                    "WI"), ("IA", "WI"), ("IA", "IL"),
+#         ("IA", "MO"), ("MO", "IL"), ("MO", "KY"), ("MO",
+#                                                    "TN"), ("MO", "AR"), ("AR", "LA"),
+#         ("AR", "MS"), ("AR", "TN"), ("LA", "MS"), ("WI",
+#                                                    "IL"), ("WI", "MI"), ("IL", "IN"),
+#         ("IL", "KY"), ("MS", "TN"), ("MS", "AL"), ("IN",
+#                                                    "MI"), ("IN", "OH"), ("IN", "KY"),
+#         ("KY", "OH"), ("KY", "WV"), ("KY", "VA"), ("KY",
+#                                                    "TN"), ("TN", "VA"), ("TN", "NC"),
+#         ("TN", "GA"), ("TN", "AL"), ("AL", "GA"), ("AL",
+#                                                    "FL"), ("GA", "FL"), ("GA", "SC"),
+#         ("GA", "NC"), ("SC", "NC"), ("NC", "VA"), ("VA",
+#                                                    "WV"), ("VA", "MD"), ("VA", "DE"),
+#         ("WV", "OH"), ("WV", "PA"), ("WV", "MD"), ("OH",
+#                                                    "PA"), ("MI", "OH"), ("ME", "NH"),
+#         ("NH", "VT"), ("NH", "MA"), ("VT", "NY"), ("VT",
+#                                                    "MA"), ("NY", "MA"), ("NY", "CT"),
+#         ("NY", "RI"), ("NY", "PA"), ("NY", "NJ"), ("MA",
+#                                                    "CT"), ("MA", "RI"), ("CT", "RI"),
+#         ("CT", "NY"), ("RI", "NY"), ("PA", "NJ"), ("DE", "NJ"), ("DE", "MD")
+#     ]
 
-def graph_coloring(graph, m):
-    colors_assigned = [-1] * len(graph)
-    if graph_coloring_util(graph, m, colors_assigned, 0):
-        return colors_assigned
-    return []
+#     for border in borders:
+#         G.add_edge(*border)
 
+#     # Color the graph
+#     color_map = nx.coloring.greedy_color(G, strategy="largest_first")
+#     node_colors = [color_map[node] for node in G.nodes()]
 
-number_of_nodes = 50
-probability_of_edge = 0.5
+#     # Draw the graph
+#     pos = nx.spring_layout(G)
+#     # nx.draw(G, pos, node_color=node_colors, with_labels=True,
+#     #         node_size=1000, cmap=plt.cm.rainbow)
+#     # Use matplotlib to draw the nodes and edges
+#     fig, ax = plt.subplots()
+#     for edge in G.edges():
+#         n1, n2 = edge
+#         x1, y1 = pos[n1]
+#         x2, y2 = pos[n2]
+#         ax.plot([x1, x2], [y1, y2], color='black')  # Edges are drawn in black
 
+#     for node, color in zip(G.nodes(), node_colors):
+#         x, y = pos[node]
+#         ax.scatter(x, y, color=plt.cm.rainbow(color), s=1000)
+#         ax.text(x, y, node)
 
-def generate_graph():
-
-    state_data = [
-        ("AK", []),
-        ("AL", ["FL", "GA", "MS", "TN"]),
-        ("AR", ["LA", "MS", "MO", "OK", "TN", "TX"]),
-        ("AZ", ["CA", "CO", "NV", "NM", "UT"]),
-        ("CA", ["AZ", "NV", "OR"]),
-        ("CO", ["KS", "NE", "NM", "OK", "UT", "WY"]),
-        ("CT", ["MA", "NY", "RI"]),
-        ("DC", ["VA", "MD"]),
-        ("DE", ["MD", "NJ", "PA"]),
-        ("FL", ["AL", "GA"]),
-        ("GA", ["AL", "FL", "NC", "SC", "TN"]),
-        ("HI", []),
-        ("IA", ["IL", "MN", "MO", "NE", "SD", "WI"]),
-        ("ID", ["MT", "NV", "OR", "UT", "WA", "WY"]),
-        ("IL", ["IA", "IN", "KY", "MO", "WI"]),
-        ("IN", ["IL", "KY", "MI", "OH"]),
-        ("KS", ["CO", "MO", "NE", "OK"]),
-        ("KY", ["IL", "IN", "MO", "OH", "TN", "VA", "WV"]),
-        ("LA", ["AR", "MS", "TX"]),
-        ("MA", ["CT", "NH", "NY", "RI", "VT"]),
-        ("MD", ["DE", "DC", "PA", "VA", "WV"]),
-        ("ME", ["NH"]),
-        ("MI", ["IN", "OH", "WI"]),
-        ("MN", ["IA", "ND", "SD", "WI"]),
-        ("MO", ["AR", "IA", "IL", "KS", "KY", "NE", "OK", "TN"]),
-        ("MS", ["AL", "AR", "LA", "TN", "TX"]),
-        ("MT", ["ID", "ND", "SD", "WY"]),
-        ("NC", ["GA", "SC", "TN", "VA"]),
-        ("ND", ["MN", "MT", "SD"]),
-        ("NE", ["CO", "IA", "KS", "MO", "SD", "WY"]),
-        ("NH", ["MA", "ME", "VT"]),
-        ("NJ", ["CT", "NY", "PA"]),
-        ("NM", ["AZ", "CO", "OK", "TX"]),
-        ("NV", ["AZ", "CA", "ID", "OR", "UT"]),
-        ("NY", ["CT", "MA", "NJ", "PA", "VT"]),
-        ("OH", ["IN", "KY", "MI", "PA", "WV"]),
-        ("OK", ["AR", "CO", "KS", "MO", "NM", "TX"]),
-        ("OR", ["CA", "ID", "NV", "WA"]),
-        ("PA", ["DE", "MD", "NJ", "NY", "OH", "WV"]),
-        ("RI", ["CT", "MA"]),
-        ("SC", ["GA", "NC"]),
-        ("SD", ["IA", "MN", "MT", "ND", "NE", "WY"]),
-        ("TN", ["AL", "AR", "GA", "KY", "MO", "MS", "NC", "VA"]),
-        ("TX", ["AR", "LA", "NM", "OK", "WA"]),
-        ("UT", ["AZ", "CO", "ID", "NV", "WY"]),
-        ("VA", ["DC", "KY", "MD", "NC", "TN", "WV"]),
-        ("VT", ["MA", "NH", "NY"]),
-        ("WA", ["ID", "OR", "OR", "TX"]),
-        ("WI", ["IA", "IL", "IN", "MI", "MN", "MO", "ND", "NE", "SD",
-                "WY"]),
-        ("WV", ["DC", "MD", "NC", "PA", "VA"]),
-        ("WY", ["CO", "ID", "MT", "ND", "SD", "UT"])]
-
-    states = [state[0] for state in state_data]
-
-    adjacency_matrix = [[0 for _ in range(len(states))]
-                        for _ in range(len(states))]
-
-    for state, neighbours in state_data:
-        for neighbour in neighbours:
-            adjacency_matrix[states.index(state)][states.index(neighbour)] = 1
-            adjacency_matrix[states.index(neighbour)][states.index(state)] = 1
-
-    return states, adjacency_matrix
+#     plt.show()
 
 
-# Assuming your graph is a 2D adjacency matrix. The below line should be replaced by the actual method to get the graph.
-_, adjacency_matrix = generate_graph()
-
-# Color the graph with a maximum of m colors
-m = 4
-colors = graph_coloring(adjacency_matrix, m)
-
-# colors = 'GOGOYYGOBGYOYYOOOGOBYOGOYYGOBBYOGGYYBBGOBYBYBGOOBOO'
-colors = 'GOGOYYOGYBYGGYBOOGOBBBBOYYGGBBYOGGYYBBGYOYBYBYOOYOO'
-
-# replace letter o in colors with y
-# colors = colors.replace('O', 'Y')
-
-# convert colors to lower case
-colors = colors.lower()
-
-# convert string into a list of characters
-colors = list(colors)
-
-print(colors)
-
-# convert colors to strings: 1 -> 'r', 2 -> 'g', 3 -> 'b', 4 -> 'y'
-# colors = list(map(lambda x: 'r' if x == 1 else 'g' if x ==
-#               2 else 'b' if x == 3 else 'y', colors))
-
-# create the map
-map = Basemap(llcrnrlon=-119, llcrnrlat=22, urcrnrlon=-64, urcrnrlat=49,
-              projection='lcc', lat_1=33, lat_2=45, lon_0=-95)
-
-# load the shapefile, use the name 'states'
-map.readshapefile('st99_d00', name='states', drawbounds=True)
-
-# collect the state names from the shapefile attributes so we can
-# look up the shape obect for a state by it's name
-state_names = []
-for shape_dict in map.states_info:
-    state_names.append(shape_dict['NAME'])
-
-ax = plt.gca()  # get current axes instance
-
-# define a list of 50 colors
-# colors = ['r', 'g', 'b', 'r', 'g', 'b', 'r', 'g', 'b', 'r',
-#           'r', 'g', 'b', 'r', 'g', 'b', 'r', 'g', 'b', 'r',
-#           'b', 'r', 'g', 'b', 'r', 'g', 'b', 'r', 'g', 'b',
-#           'r', 'g', 'b', 'r', 'g', 'b', 'r', 'g', 'b', 'r',
-#           'r', 'g', 'b', 'r', 'g', 'b', 'r', 'g', 'b', 'r'
-#           ]
-# get Texas and draw the filled polygon
-
-STATES = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois',
-          'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey',
-          'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma',
-          'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota',
-          'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington',
-          'West Virginia', 'Wisconsin', 'Wyoming']
-
-for state in STATES:
-    seg = map.states[state_names.index(state)]
-    if colors[STATES.index(state)] == 'o':
-        poly = Polygon(seg, facecolor='orange', edgecolor='black')
-    else:
-        poly = Polygon(
-            seg, facecolor=colors[STATES.index(state)], edgecolor='black')
-    ax.add_patch(poly)
-# seg = map.states[state_names.index('Texas')]
-# poly = Polygon(seg, facecolor='red', edgecolor='red')
-# ax.add_patch(poly)
-
-plt.show()
+# draw_colored_us_map()
+###
