2b7d2949d0d3ce6cead73b44bee45cfb37984e5d
hlin863 <zcabhli@ucl.ac.uk>
2023-09-21 14:08:58 +0100

    Colored States with genetic algorithm
    Updated draw.py

--- draw.py
+++ draw.py
@@ -1,6 +1,29 @@
+from genetic import *
+import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap
 from matplotlib.patches import Polygon
+
+number_of_nodes = 50
+probability_of_edge = 0.5
+population_size = 100
+n_generations = 50
+
+_, adjacency_list = generate_random_graph(number_of_nodes, probability_of_edge)
+
+initial_population = generate_random_initial_population(
+    population_size, number_of_nodes, adjacency_list)
+
+results_fitness, results_fittest = evolution(
+    initial_population, n_generations, population_size)
+
+visualize_results(results_fitness, results_fittest)
+
+# Display the colors of the states for the fittest individual of the last generation
+print("Colors of states for the fittest individual of the last generation:")
+print(results_fittest[-1].colors)
+
+colors = results_fittest[-1].colors
 
 # create the map
 map = Basemap(llcrnrlon=-119, llcrnrlat=22, urcrnrlon=-64, urcrnrlat=49,
@@ -18,12 +41,12 @@ for shape_dict in map.states_info:
 ax = plt.gca()  # get current axes instance
 
 # define a list of 50 colors
-colors = ['r', 'g', 'b', 'r', 'g', 'b', 'r', 'g', 'b', 'r',
-          'r', 'g', 'b', 'r', 'g', 'b', 'r', 'g', 'b', 'r',
-          'b', 'r', 'g', 'b', 'r', 'g', 'b', 'r', 'g', 'b',
-          'r', 'g', 'b', 'r', 'g', 'b', 'r', 'g', 'b', 'r',
-          'r', 'g', 'b', 'r', 'g', 'b', 'r', 'g', 'b', 'r'
-          ]
+# colors = ['r', 'g', 'b', 'r', 'g', 'b', 'r', 'g', 'b', 'r',
+#           'r', 'g', 'b', 'r', 'g', 'b', 'r', 'g', 'b', 'r',
+#           'b', 'r', 'g', 'b', 'r', 'g', 'b', 'r', 'g', 'b',
+#           'r', 'g', 'b', 'r', 'g', 'b', 'r', 'g', 'b', 'r',
+#           'r', 'g', 'b', 'r', 'g', 'b', 'r', 'g', 'b', 'r'
+#           ]
 
 print(len(colors))
 # get Texas and draw the filled polygon
